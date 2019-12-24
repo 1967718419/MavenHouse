fd707d925c24e3cd08b42977c36c061d149397db
unknown <1967718419@qq.com>
2019-12-24 10:31:24 +0800

    commit

---
+++ z_hous/src/main/java/com/hous/service/user/LoginService.java
@@ -0,0 +1,23 @@
+package com.hous.service.user;
+
+import java.util.List;
+
+import com.hous.pojo.User;
+import com.hous.pojo.Userlist;
+
+public interface LoginService {
+
+	User checkLogin(User user);
+
+	User register(User user);
+
+	boolean insertUser(User user);
+
+	User checklg(User user);
+
+
+
+
+	
+
+}
